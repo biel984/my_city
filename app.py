26136ac8e96640fca74a5e00d48f0c40eb756232
biel984 <gdrgdiokkj@gmail.com>
2025-09-09 14:21:35 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+
+st.title("meus projetos🤬")
+st.caption("criando com streamlit😡")
+st.image("https://i.pinimg.com/1200x/ab/e5/fc/abe5fc1b7ca33d8b59fbbc6c85583d0d.jpg")
+
+st.markdown(
+"""
+### carros a venda!
+*italico*\n
+**negrito**
+"""
+)
